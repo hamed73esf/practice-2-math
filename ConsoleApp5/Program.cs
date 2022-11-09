1095c22ef656d3feab15f14fc22bd649e89d207d
hamed <blueNoteBook@DESKTOP-2OV0I4P>
2022-11-09 12:10:20 +0330

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class test
+{
+    static void Main()
+    {
+        Console.WriteLine("enter x:");
+        string x = Console.ReadLine();
+        Console.WriteLine("enter y:");
+        string y = Console.ReadLine();
+        int a = Convert.ToInt32(x);
+        int b = Convert.ToInt32(y);
+        double c = Math.Sqrt(Math.Pow(Math.Abs(a - b), Math.Abs(b)));
+        Console.WriteLine(c);
+    }
+}
